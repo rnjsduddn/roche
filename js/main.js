ced610b906525f81becde6a5c6a0150731862006
rnjsduddn <91304854+rnjsduddn@users.noreply.github.com>
2021-11-08 09:52:19 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// main.js
+$(function(){
+  // 2단계 메뉴
+  $('#gnb .depth1 > li').hover(
+    function(){
+      $(this)
+        .find('.depth2')
+        .stop()
+        .slideDown(300);
+    },
+    function(){
+      $(this)
+        .find('.depth2')
+        .stop()
+        .slideUp(300);
+    }
+  );
+
+  // 검색창 열기 버튼
+  $('#main-header .search > a').click(function(){
+    $('#search-popup').show();
+  });
+  
+  // 검색창 닫기
+  $('#btn-close').click(function(){
+    $('#search-popup').hide();
+  })
+})
